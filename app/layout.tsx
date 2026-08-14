91b8ce9555c5989ce0f07b9e80f7ec3618063d98
Daryhen-dev <daryhen.work@gmail.com>
2026-08-14 17:22:04 -0500

    feat(ui): add sonner toaster mounted top-right in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Toaster } from "@/components/ui/sonner";
 import { APPLICATION_METADATA_MESSAGES, USER_LOCALE } from "@/lib/localization/es-ec";
 import "./globals.css";
 
@@ -24,7 +25,10 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang={USER_LOCALE}
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Toaster />
+      </body>
     </html>
   );
 }
